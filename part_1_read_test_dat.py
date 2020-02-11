47361842ae65ebc1c983d40ff865c05ba392d741
xluo1 <xluo1@andrew.cmu.edu>
2020-02-11 12:12:45 -0500

    Created pfgd_test.txt

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,4 +5,7 @@ input_dat_file = "data/pfgd_test.dat"
 
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+cc_dat = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
 #print the resulting data
+print(cc_dat)
