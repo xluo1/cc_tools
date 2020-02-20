8da9c3113b80f6344786dd3e9d1cd5146341a00b
xluo1 <xluo1@andrew.cmu.edu>
2020-02-20 15:54:54 -0500

    Update before downloading bug fix

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -3,4 +3,11 @@ import cc_dat_utils
 #Part 3
 #Load your custom JSON file
 #Convert JSON data to CCLevelPack
+
+# 1) break apart levels into CCLevel
+# 2) Parse the optional fields and put them in these classes:
+# map title, encoded PW, hint, monster movement
+# 3) Put these optional fields in CCLevel
+# 4) put CCLevels into CCLevelPack
+
 #Save converted data to DAT file
